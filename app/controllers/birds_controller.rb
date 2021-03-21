76f9c7e52afbbe65132d5a0d105ee21a4eca072c
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-21 12:53:32 -0500

    commit tree 2a3ba94954a5388e8595d10be4d3738dfe9762b7

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,8 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    # render json: {message: "Hello #{@birds[3].name}"}
+    render json: {birds: birds, messages: ["Yehuda is awesome", "nice"]}
+    # render 'birds/index.html.erb'
   end
 end
